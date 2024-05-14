059a3f8a412ee992e0416bba6bb4e16be8c52518
Wellington-100 <vasile.andruscenco@gmail.com>
2024-05-14 18:29:58 +0300

    toFixed()...

--- index.mjs
+++ index.mjs
@@ -1,7 +1,24 @@
+// user imputs 3 rating values
+// the script calculates the avg rating
+// outputs the avg
 
-import renderNews from './ui.mjs'
-import getAllNews from './storage.mjs'
+import readline from 'node:readline'
 
-let news = await getAllNews()
+let io = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
-renderNews(news)
+io.question('enter 3 rating values separated by space: ', answer => {
+    let rating = answer.split(' ').map(value => parseFloat(value))
+
+    // HW2: calculate this, using standard for()
+    // HW3: calculate this, using Array.reduce()
+    let avgValue = (rating[0] + rating[1] + rating[2]) / 3
+
+    // HW1: only 1 digit...
+    let avgRating = avgValue.toFixed(1)
+
+    console.log('the average rating is: ', avgRating)
+    io.close()
+})
